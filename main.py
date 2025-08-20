1b8c69f3d5c5d00a3c0161d57c0eb317346f875f
sandeep <jayasandeepr@gmail.com>
2025-08-20 21:25:38 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return a + b
+
+def main():
+    x = 10
+    y = 20
+    result = add(x, y)
+    print("The sum of", x, "and", y, "is:", result)
+
+if __name__ == "__main__":
+    main()
