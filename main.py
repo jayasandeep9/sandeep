04d9d26674f99aa52c51583c49a6a5f4d232e9d0
sandeep <jayasandeepr@gmail.com>
2025-08-20 21:38:18 +0530

    hmtl.txt

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ def add(a, b):
     return a + b
 
 def main():
-    x = 10
+    x = 22
     y = 20
     result = add(x, y)
     print("The sum of", x, "and", y, "is:", result)
 
 if __name__ == "__main__":
     main()
+    
